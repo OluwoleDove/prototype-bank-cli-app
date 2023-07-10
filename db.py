eb09e056f14c8385222bf421802aa80991e84295
OluwoleDove <ladmans07@gmail.com>
2023-07-10 17:25:10 +0100

    testing something

--- db.py
+++ db.py
@@ -1,11 +1,12 @@
 import mysql.connector
+from db_details import db
 
 ############################# DATABASE CODE #######################
 def create_db():
     mydb = mysql.connector.connect(
-        host="localhost",
-        user="",
-        password="",
+        host = "localhost",
+        user = db["user"],
+        password = db["password"],
         database="bank_cli_db" #returns an error if DB does not exist
     )
     
