837a985f578437c6eab5c6a59fae1d35202db3f8
OluwoleDove <ladmans07@gmail.com>
2023-08-16 22:32:05 +0100

    updating bank cli repo

--- bank_mix.py
+++ bank_mix.py
@@ -10,9 +10,14 @@ def bank_mix():
         acc_type = "Savings"
     elif option == "2":
         acc_type = "Current"
+    else:
+        print("Wrong entry! Please try again.")
+        bank_mix()
     
     tnx_type = input("\nSelect transaction type: \n1. Create Account \n2. Change Pin\n3. Check Balance\n4. Deposit\n5. Withdrawal\n6. Transfer\n7. Close Account\n\n")
     tnx_dict = {"1":"create_account", "2":"change_pin", "3":"check_balance", "4":"deposit", "5":"withdrawal", "6":"funds_transfer", "7":"close_account"}
     
-    this_customer = input_prompt(users_tab)
+    if tnx_type == 1:
+        this_customer = input_prompt(users_tab)
+        
     process_transactions(this_customer, acc_type, tnx_dict[tnx_type])
