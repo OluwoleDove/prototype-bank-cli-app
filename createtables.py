0bda080b768c1ee146eb2993c9a5b02446c61f32
OluwoleDove <ladmans07@gmail.com>
2023-08-20 02:44:53 +0100

    Database connection made and tables created

--- createtables.py
+++ createtables.py
@@ -1,12 +1,41 @@
-from db import create_db 
+from db import mydb, new_db
 
-new_db = create_db.cursor()
+#Create the 'users' table
+new_db.execute("CREATE TABLE users ("
+               "id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, "
+               "firstname VARCHAR(50) NOT NULL, "
+               "lastname VARCHAR(50) NOT NULL, "
+               "email VARCHAR(50) NOT NULL, "
+               "phone VARCHAR(20) NOT NULL, "
+               "gender ENUM('Male', 'Female', 'Prefer not to say') DEFAULT 'Prefer not to say', "
+               "dob DATETIME, "
+               "occupation VARCHAR(50), "
+               "city VARCHAR(50), "
+               "join_date DATETIME"
+               ")")
 
-#CREATE TABLES - users  |  accounts  |  Table transactions
-new_db.execute("CREATE TABLE users (id INT(11) NOT NULL AUTO_INCREMENT, firstname VARCHAR(50) NOT NULL, lastname VARCHAR(50) NOT NULL, email VARCHAR(50), phone VARCHAR(20), gender VARCHAR (10), dob DATETIME, occupation VARCHAR(50), city VARCHAR(50), join_date DATETIME PRIMARY KEY (id))")
-new_db.execute("CREATE TABLE accounts (id INT(11) NOT NULL AUTO_INCREMENT, user_id INT(11) NOT NULL, account_type ENUM('Savings', 'Current', 'Fixed') DEFAULT 'Savings', balance FLOAT(10,2), charges FLOAT(10,2), interests FLOAT(10,2), pin INT(4), last_edited DATETIME PRIMARY KEY (user_id))")
-new_db.execute("CREATE TABLE transactions (id INT(11) NOT NULL AUTO_INCREMENT, user_id INT(11) NOT NULL, transaction_type ENUM('Create_Account', 'Deposit', 'Withdrawal', 'Transfer', 'Pin_Change', 'Close_Account') DEFAULT 'Create_Account', transaction_amount FLOAT(10,2), transaction_date DATETIME PRIMARY KEY (user_id))")
-create_db.commit()
+#Create the 'accounts' table
+new_db.execute("CREATE TABLE accounts ("
+               "id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, "
+               "user_id INT(11) NOT NULL, "
+               "account_type ENUM('Savings', 'Current', 'Fixed') DEFAULT 'Savings', "
+               "balance FLOAT(10,2), "
+               "charges FLOAT(10,2), "
+               "interests FLOAT(10,2), "
+               "pin INT(4), "
+               "last_edited DATETIME"
+               ")")
 
-new_db.execute("SHOW TABLES")
+#Create the 'transactions' table
+new_db.execute("CREATE TABLE transactions ("
+               "id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, "
+               "user_id INT(11) NOT NULL, "
+               "transaction_type ENUM('Create_Account', 'Deposit', 'Withdrawal', 'Transfer', 'Pin_Change', 'Close_Account') DEFAULT 'Create_Account', "
+               "transaction_amount FLOAT(10,2), "
+               "transaction_date DATETIME"
+               ")")
+
+mydb.commit()
+
+print(new_db, "Tables created successfully.")
 ######################################################################
--- db.py
+++ db.py
@@ -1,13 +1,13 @@
 import mysql.connector
 from db_params import db_params
 
-############################# DATABASE CODE #######################
-def create_db():
-    mydb = mysql.connector.connect(
-        host = "localhost",
-        user = db_params["user"],
-        password = db_params["password"],
-        database="bank_cli_db" #returns an error if DB does not exist
-    )
+############################# DATABASE CONNECTION CODE #######################
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = db_params["user"],
+    password = db_params["password"],
+    database="bank_cli_db" #returns an error if DB does not exist
+)
+
+new_db = mydb.cursor()
     
-    return mydb
