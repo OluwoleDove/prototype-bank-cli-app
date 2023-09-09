e1fb6cc1515af7ddbd513879efe683ae3e8a8c34
OluwoleDove <ladmans07@gmail.com>
2023-09-09 21:57:26 +0100

    changed python root directory for subprocess

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import subprocess
 
 def run_cronjob():
     print("Running cron job...")
-    subprocess.run(["/usr/bin/python3", "cronjob.py"])
+    subprocess.run(["C:/Python311/Python", "cronjob.py"])
     print("Cron job completed.")
 
 # Schedule the cron job to run daily at midnight
