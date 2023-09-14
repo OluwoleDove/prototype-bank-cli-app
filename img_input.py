aa967aa8eae3ebfcc925e4093667685724e10d73
OluwoleDove <ladmans07@gmail.com>
2023-09-14 23:25:48 +0100

    image upload requirements satisfied

--- img_input.py
+++ img_input.py
@@ -14,27 +14,29 @@ def get_image_path_from_dialog():
 def generate_random_string(length):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
-def save_image_with_random_name(image_path, save_folder, user_name):
+def save_image_with_random_name(image_path, base_folder, user_name):
     try:
         if not os.path.isfile(image_path):
             print("The specified file does not exist.")
             return
 
-        if not os.path.exists(save_folder):
-            os.makedirs(save_folder)
+        # Create a folder inside the base folder with the user's name
+        user_folder = os.path.join(base_folder, user_name)
+        if not os.path.exists(user_folder):
+            os.makedirs(user_folder)
 
         image_name = os.path.basename(image_path)
         file_name, file_extension = os.path.splitext(image_name)
 
-        # Generate 12 random characters
-        random_chars = generate_random_string(12)
+        # Generate 20 random characters
+        random_chars = generate_random_string(20)
 
-        # Customize the new file name using the user's name and random characters
-        new_file_name = f"{user_name}_{random_chars}{file_extension}"
-        save_path = os.path.join(save_folder, new_file_name)
+        # Customize the new file name with random characters and the image extension
+        new_file_name = f"{random_chars}{file_extension}"
+        save_path = os.path.join(user_folder, new_file_name)
 
         shutil.copyfile(image_path, save_path)
-        print(f"Image saved as {new_file_name} in {save_folder}")
+        print(f"Image saved as {new_file_name} in {user_folder}")
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
@@ -42,6 +44,6 @@ def save_image_with_random_name(image_path, save_folder, user_name):
 if __name__ == "__main__":
     user_name = input("Enter your name: ")
     image_path = get_image_path_from_dialog()
-    save_folder = "avatar"
+    base_folder = "avatar"
 
-    save_image_with_random_name(image_path, save_folder, user_name)
+    save_image_with_random_name(image_path, base_folder, user_name)
